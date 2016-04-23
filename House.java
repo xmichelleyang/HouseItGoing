df35479e022aaf9ce335f0746ab7cb1c8f19ef90
Michelle Yang <zomgitsmichelle@gmail.com>
2016-04-23 16:51:13 -0700

    Modified saveCSV.java

--- House.java
+++ House.java
@@ -1,12 +1,15 @@
 
 
 public class House extends Properties {
+   String address;
 
    //Constructor
-   House (int total_bedrooms, int open_bedrooms, int bathrooms, float distance,
+   House (String address, int total_bedrooms, int open_bedrooms, int bathrooms, float distance,
          float max_price, int sqft) {
             super(total_bedrooms, open_bedrooms, bathrooms, distance, max_price, sqft, null);
+            this.address = address;
             this.type = "House";
    }
 
-}
+
+   }
--- saveCSV.java
+++ saveCSV.java
@@ -1,51 +1,64 @@
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.StringWriter;
+import java.io.PrintStream;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.regex;
 
-import au.com.bytecode.opencsv.CSVReader;
+import com.opencsv.CSVReader;
 
 public class saveCSV {
 
    public static void main(String[] args) {
       //create CSVReader object
-      CSVReader reader = new CSVReader(new FileReader("housing.csv"), ',');
+      try {
+         CSVReader reader = new CSVReader(new FileReader("cleaned_housing.csv"), ',');
 
-      List <House> houses = new ArrayList<House>();
 
-      //Read line by line
-      String[] record = null;
+         List <House> houses = new ArrayList<House>();
 
-      while ((record = reader.readNext()) != null) {
-         String address = record[0];
+         //Read line by line
+         String[] record = null;
          try {
-            int total_bedrooms = Integer.parseInt(record[1]);
-            int open_bedrooms = Integer.parseInt(record[2]);
-            int bathrooms = Integer.parseInt(record[3]);
-            float distance = Float.parseFloat(record[4]);
-            float max_price = Float.parseFloat(record[5]);
-            int sqft = Integer.parseInt(record[6]);
+            while ((record = reader.readNext()) != null) {
+               try {
+                  String address = record[0];
+                  int total_bedrooms = Integer.parseInt(record[1]);
+                  int open_bedrooms = Integer.parseInt(record[2]);
+                  int bathrooms = Integer.parseInt(record[3]);
+                  float distance = Float.parseFloat(record[4]);
+                  float max_price = Float.parseFloat(record[5]);
+                  int sqft = Integer.parseInt(record[6]);
 
-            //if add parsing was done without error, create and add house
-            House house = new House(address, total_bedrooms, open_bedrooms,
-                                    bathrooms, distance, max_price, sqft);
-            houses.add(house);
+                  //if add parsing was done without error, create and add house
+                  House house = new House(address, total_bedrooms, open_bedrooms,
+                                          bathrooms, distance, max_price, sqft);
+                  houses.add(house);
+               }
+               catch (NumberFormatException e) {
+                  System.err.println("Invalid integer or float value inputted");
+               }
+               catch (NullPointerException e1) {
+                  System.err.println("String for parseFloat is null");
+               }
+
+            }
+            reader.close();
+
          }
-         catch (NumberFormatException e) {
-            System.err.println("Invalid integer or float value inputted");
+         catch (IOException e) {
+            System.err.println("IOException");
          }
-         catch (NullPointerException e1) {
-            System.out.println("String for parseFloat is null");
-         }
+         System.out.println("hello" + houses);
 
       }
-      reader.close();
-      System.out.println("hello" + houses);
+      catch (FileNotFoundException e2) {
+         System.err.println("File not found");
+      }
    }
-
-
 }
