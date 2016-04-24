3f3f3120bcf6abe0e7431f0161b3e26ceea4fa91
Alexander Tran <exand.tran@berkeley.edu>
2016-04-24 05:54:04 -0700

    Update Landlord.java

--- app/src/main/java/com/example/griffin/housematch/Landlord.java
+++ app/src/main/java/com/example/griffin/housematch/Landlord.java
@@ -1,44 +1,64 @@
 package com.example.griffin.housematch;
 
+import android.content.Context;
 import android.content.Intent;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Landlord extends Activity {
+
+    EditText address, total_bedrooms, open_bedrooms, bathrooms, distance, sqft, price, email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landlord);
 
-        String address = ((EditText)findViewById(R.id.editText)).getText().toString();
-        String total_bedrooms = ((EditText)findViewById(R.id.editText2)).getText().toString();
-        String open_bedrooms = ((EditText)findViewById(R.id.editText3)).getText().toString();
-        String bathrooms = ((EditText)findViewById(R.id.editText4)).getText().toString();
-        String distance = ((EditText)findViewById(R.id.editText5)).getText().toString();
-        String sqft = ((EditText)findViewById(R.id.editText6)).getText().toString();
-        String price = ((EditText)findViewById(R.id.totalroomrank)).getText().toString();
-        String email = ((EditText)findViewById(R.id.openroomrank)).getText().toString();
+        this.address = (EditText)findViewById(R.id.editText);
+        this.total_bedrooms = (EditText)findViewById(R.id.editText2);
+        this.open_bedrooms = (EditText)findViewById(R.id.editText3);
+        this.bathrooms = (EditText)findViewById(R.id.editText4);
+        this.distance = (EditText)findViewById(R.id.editText5);
+        this.sqft = (EditText)findViewById(R.id.editText6);
+        this.price = (EditText)findViewById(R.id.editText7);
+        this.email = (EditText)findViewById(R.id.editText8);
+
+    }
+    public void toMainScreen(View view) {
+        String address = this.address.getText().toString();
+        String total_bedrooms = this.total_bedrooms.getText().toString();
+        String open_bedrooms = this.open_bedrooms.getText().toString();
+        String bathrooms = this.bathrooms.getText().toString();
+        String distance = this.distance.getText().toString();
+        String sqft = this.sqft.getText().toString();
+        String price = this.price.getText().toString();
+        String email = this.email.getText().toString();
 
         House house = new House(address, Float.parseFloat(total_bedrooms),
                 Float.parseFloat(open_bedrooms), Float.parseFloat(bathrooms),
                 Float.parseFloat(distance), Float.parseFloat(price),
                 Integer.parseInt(sqft), email);
 
-        try{house.save();}
-        catch(IOException e) {
-            System.out.println("Cannot find file.");
+        String filename = "housing_data.csv";
+        FileOutputStream outputStream;
+        String contents = house.save();
+
+        try {
+            outputStream = openFileOutput(filename, Context.MODE_APPEND);
+            outputStream.write(contents.getBytes());
+            outputStream.close();
+            System.out.println("Successfully saved housing data");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-    }
-    public void toMainScreen(View view) {
         Intent intent = new Intent(Landlord.this, MainScreen.class);
         startActivity(intent);
-
     }
 
 }
