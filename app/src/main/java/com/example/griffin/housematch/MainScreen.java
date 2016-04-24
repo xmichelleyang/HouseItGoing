aed76a1ef2b1949368d80b42affe42d8c5d37373
Griffin Stamp <gstamp@san.rr.com>
2016-04-24 03:33:45 -0700

    location select

--- app/src/main/java/com/example/griffin/housematch/MainScreen.java
+++ app/src/main/java/com/example/griffin/housematch/MainScreen.java
@@ -4,15 +4,42 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainScreen extends AppCompatActivity {
-
+    String loc = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        Spinner spinner = (Spinner) findViewById(R.id.locSelect);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapter, View v,
+                                       int position, long id) {
+                // On selecting a spinner item
+                loc = adapter.getItemAtPosition(position).toString();
+                // Showing selected spinner item
+                Toast.makeText(getApplicationContext(),
+                        "Selected Country : " + loc, Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // TODO Auto-generated method stub
+            }
+        });
+
     }
+
     public void toTenant(View view) {
+
         Intent intent = new Intent(MainScreen.this, TenantPref.class);
         startActivity(intent);
     }
