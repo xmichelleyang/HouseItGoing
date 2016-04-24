e196cbd3546dc5c3d224198c5ad72da3f5d08b5e
Michelle Yang <zomgitsmichelle@gmail.com>
2016-04-24 01:57:06 -0700

    Added Android features

---
+++ tester.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+
+public class tester {
+
+   public static void main(String[] args) throws IOException {
+         House house = new House ("4545 Swag", 1,1,1,500,600,500 );
+         house.save();
+         House ayea = new House ("4545 DJSIOFJWRSwag", 111,11,1,5100,6100,5010 );
+         ayea.save();
+
+   }
+}
